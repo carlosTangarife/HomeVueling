5360b9ca89ee375e710477c0149cf69b9433144c
andresfm87@gmail.com <andresfm87@gmail.com>
2017-08-18 07:46:07 -0500

    PRJ-5138: The main component of the search tag is included in the index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule, DOCUMENT } from '@angular/platform-browser';
 import { NgModule, Inject, ApplicationRef } from '@angular/core';
+import { SearchModule } from './search/search.module';
+import { SearchComponent } from './search/components/index-search/search.component';
 
 @NgModule({
   declarations: [
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SearchModule
   ],
   providers: [
 
@@ -15,8 +18,7 @@ import { NgModule, Inject, ApplicationRef } from '@angular/core';
 export class AppModule {
   private browser_document;
   ngDoBootstrap(appRef: ApplicationRef ) {
-    if (this.browser_document.getElementsByTagName('app-index-search').length > 0) { appRef.bootstrap(IndexSearchComponent); }
-    if (this.browser_document.getElementsByTagName('app-index-header').length > 0) { appRef.bootstrap(IndexHeaderComponent); }
+    if (this.browser_document.getElementsByTagName('app-search').length > 0) { appRef.bootstrap(SearchComponent); }
   }
   constructor(@Inject(DOCUMENT) private document: any) {
     this.browser_document = document;
