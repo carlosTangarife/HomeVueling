c5eb9011b90cd81ea5f4377d885ef52c83e953d9
andresfm87@gmail.com <andresfm87@gmail.com>
2017-08-18 12:13:39 -0500

    PRJ-5138: was added index that content all services of searchComponent

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { SEARCH_COMPONENTS } from './index';
+import { StationSelectorComponent } from './components/station-selector/station-selector.component';
+import { SEARCH_SERVICES } from './services/index';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
-    ...SEARCH_COMPONENTS
+    ...SEARCH_COMPONENTS,
+    StationSelectorComponent
   ],
   exports: [
-    ...SEARCH_COMPONENTS
+    // ...SEARCH_COMPONENTS
   ],
   providers: [
-
+    SEARCH_SERVICES
   ],
   entryComponents: [SEARCH_COMPONENTS]
 })
