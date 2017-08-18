61b481d016eaeb39ef167acd3157dd6c1905c3af
andresfm87@gmail.com <andresfm87@gmail.com>
2017-08-18 06:57:56 -0500

    PRJ-5135: The inner internal structure of the search component is redefined, so that it is reusable

---
+++ src/app/search/index.ts
@@ -0,0 +1,11 @@
+import { SearchComponent } from './components/index-search/search.component';
+import { CheckInComponent } from './components/check-in/check-in.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { FlightComponent } from './components/flight/flight.component';
+
+export const SEARCH_COMPONENTS = [
+  SearchComponent,
+  CheckInComponent,
+  ReservationComponent,
+  FlightComponent
+];
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchComponent } from './search.component';
-import { CheckInComponent } from './components/check-in/check-in.component';
-import { ReservationComponent } from './components/reservation/reservation.component';
-import { FlightComponent } from './components/flight/flight.component';
+import { SEARCH_COMPONENTS } from './index';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SearchComponent, CheckInComponent, ReservationComponent, FlightComponent],
-  providers: [{ provide: 'components', useValue: [SearchComponent], multi: true }],
-  entryComponents: [SearchComponent]
+  declarations: [
+    ...SEARCH_COMPONENTS
+  ],
+  exports: [
+    ...SEARCH_COMPONENTS
+  ],
+  providers: [
+
+  ],
+  entryComponents: [SEARCH_COMPONENTS]
 })
 export class SearchModule { }
