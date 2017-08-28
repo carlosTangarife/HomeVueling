0b5a94aa0a0cab1b4ef00925461fe3de8a9057b2
andresfm87@gmail.com <andresfm87@gmail.com>
2017-08-28 15:30:52 -0500

    PRJ-5138: New stationResent data source is created

---
+++ src/app/shared/services/stationResent.ts
@@ -0,0 +1,22 @@
+export const STATION_RESENT = {
+  stationResent: [
+    {
+      countryName: 'España',
+      countryCode: 'ES',
+      iataName: 'A Coruña',
+      iataCode: 'LCG'
+    },
+    {
+      countryName: 'Dinamarca',
+      countryCode: 'DK',
+      iataName: 'A alborg',
+      iataCode: 'AAL'
+    },
+    {
+      countryName: 'España',
+      countryCode: 'ES',
+      iataName: 'Alicante',
+      iataCode: 'ALC'
+    }
+  ]
+};
