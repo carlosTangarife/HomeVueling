17ceb5ced199d33bb8f8635f4344a7549e79335c
andresfm87@gmail.com <andresfm87@gmail.com>
2017-08-28 15:07:24 -0500

    PRJ-5138: New station data source is created

---
+++ src/app/shared/services/stationInfo.ts
@@ -0,0 +1,846 @@
+export const STATION_INFO = {
+  'stationInfo': {
+    'StationList': [
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'A Coruña',
+        iataCode: 'LCG'
+      },
+      {
+        countryName: 'Dinamarca',
+        countryCode: 'DK',
+        iataName: 'A alborg',
+        iataCode: 'AAL'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Alicante',
+        iataCode: 'ALC'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Almería',
+        iataCode: 'LEI'
+      },
+      {
+        countryName: 'PaísesBajos',
+        countryCode: 'NL',
+        iataName: 'Ámsterdam',
+        iataCode: 'AMS'
+      },
+      {
+        countryName: 'Argelia',
+        countryCode: 'DZ',
+        iataName: 'Argel',
+        iataCode: 'ALG'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Asturias(Oviedo)',
+        iataCode: 'OVD'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Atenas',
+        iataCode: 'ATH'
+      },
+      {
+        countryName: 'Gambia',
+        countryCode: 'GM',
+        iataName: 'Banjul',
+        iataCode: 'BJL'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Barcelona',
+        iataCode: 'BCN'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Bari',
+        iataCode: 'BRI'
+      },
+      {
+        countryName: 'Suiza',
+        countryCode: 'CH',
+        iataName: 'Basilea',
+        iataCode: 'BSL'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Bastia(Córcega)',
+        iataCode: 'BIA'
+      },
+      {
+        countryName: 'Líbano',
+        countryCode: 'LB',
+        iataName: 'Beirut',
+        iataCode: 'BEY'
+      },
+      {
+        countryName: 'Serbia',
+        countryCode: 'RS',
+        iataName: 'Belgrado',
+        iataCode: 'BEG'
+      },
+      {
+        countryName: 'Noruega',
+        countryCode: 'NO',
+        iataName: 'Bergen',
+        iataCode: 'BGO'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Berlín(Tegel)',
+        iataCode: 'TXL'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Bilbao',
+        iataCode: 'BIO'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Birmingham',
+        iataCode: 'BHX'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Bolonia',
+        iataCode: 'BLQ'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Brest',
+        iataCode: 'BES'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Bríndisi',
+        iataCode: 'BDS'
+      },
+      {
+        countryName: 'Bélgica',
+        countryCode: 'BE',
+        iataName: 'Bruselas',
+        iataCode: 'BRU'
+      },
+      {
+        countryName: 'Rumanía',
+        countryCode: 'RO',
+        iataName: 'Bucarest',
+        iataCode: 'OTP'
+      },
+      {
+        countryName: 'Hungría',
+        countryCode: 'HU',
+        iataName: 'Budapest',
+        iataCode: 'BUD'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Burdeos',
+        iataCode: 'BOD'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Cagliari',
+        iataCode: 'CAG'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Cardiff',
+        iataCode: 'CWL'
+      },
+      {
+        countryName: 'Marruecos',
+        countryCode: 'MA',
+        iataName: 'Casablanca',
+        iataCode: 'CMN'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Catania',
+        iataCode: 'CTA'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Cefalonia',
+        iataCode: 'EFL'
+      },
+      {
+        countryName: 'Rumanía',
+        countryCode: 'RO',
+        iataName: 'Cluj-Napoca',
+        iataCode: 'CLJ'
+      },
+      {
+        countryName: 'Dinamarca',
+        countryCode: 'DK',
+        iataName: 'Copenhague',
+        iataCode: 'CPH'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Corfú',
+        iataCode: 'CFU'
+      },
+      {
+        countryName: 'Polonia',
+        countryCode: 'PL',
+        iataName: 'Cracovia',
+        iataCode: 'KRK'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Creta',
+        iataCode: 'HER'
+      },
+      {
+        countryName: 'Senegal',
+        countryCode: 'SN',
+        iataName: 'Dakar',
+        iataCode: 'DKR'
+      },
+      {
+        countryName: 'Irlanda',
+        countryCode: 'IE',
+        iataName: 'Dublín',
+        iataCode: 'DUB'
+      },
+      {
+        countryName: 'Croacia',
+        countryCode: 'HR',
+        iataName: 'Dubrovnik',
+        iataCode: 'DBV'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Düsseldorf',
+        iataCode: 'DUS'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Edimburgo',
+        iataCode: 'EDI'
+      },
+      {
+        countryName: 'Países Bajos',
+        countryCode: 'NL',
+        iataName: 'Eindhoven',
+        iataCode: 'EIN'
+      },
+      {
+        countryName: 'Suecia',
+        countryCode: 'SE',
+        iataName: 'Estocolmo',
+        iataCode: 'ARN'
+      },
+      {
+        countryName: 'Portugal',
+        countryCode: 'PT',
+        iataName: 'Faro',
+        iataCode: 'FAO'
+      },
+      {
+        countryName: 'Marruecos',
+        countryCode: 'MA',
+        iataName: 'Fez',
+        iataCode: 'FEZ'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Florencia',
+        iataCode: 'FLR'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Fuerteventura',
+        iataCode: 'FUE'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Génova',
+        iataCode: 'GOA'
+      },
+      {
+        countryName: 'Suiza',
+        countryCode: 'CH',
+        iataName: 'Ginebra',
+        iataCode: 'GVA'
+      },
+      {
+        countryName: 'Suecia',
+        countryCode: 'SE',
+        iataName: 'Gotemburgo',
+        iataCode: 'GOT'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'GranCanaria',
+        iataCode: 'LPA'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Granada',
+        iataCode: 'GRX'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Hamburgo',
+        iataCode: 'HAM'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Hanóver',
+        iataCode: 'HAJ'
+      },
+      {
+        countryName: 'Finlandia',
+        countryCode: 'FI',
+        iataName: 'Helsinki',
+        iataCode: 'HEL'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Ibiza',
+        iataCode: 'IBZ'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Jerez(Cádiz)',
+        iataCode: 'XRY'
+      },
+      {
+        countryName: 'Rusia',
+        countryCode: 'RU',
+        iataName: 'Kaliningrado',
+        iataCode: 'KGD'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Kárpatos',
+        iataCode: 'AOK'
+      },
+      {
+        countryName: 'Ucrania',
+        countryCode: 'UA',
+        iataName: 'Kiev',
+        iataCode: 'IEV'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Kos',
+        iataCode: 'KGS'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'LaPalma',
+        iataCode: 'SPC'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Lampedusa',
+        iataCode: 'LMP'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Lanzarote',
+        iataCode: 'ACE'
+      },
+      {
+        countryName: 'Chipre',
+        countryCode: 'CY',
+        iataName: 'Lárnaca',
+        iataCode: 'LCA'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Lille',
+        iataCode: 'LIL'
+      },
+      {
+        countryName: 'Portugal',
+        countryCode: 'PT',
+        iataName: 'Lisboa',
+        iataCode: 'LIS'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Londres(Gatwick)',
+        iataCode: 'LGW'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Londres(Heathrow)',
+        iataCode: 'LHR'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Londres(Luton)',
+        iataCode: 'LTN'
+      },
+      {
+        countryName: 'Luxemburgo',
+        countryCode: 'LU',
+        iataName: 'Luxemburgo',
+        iataCode: 'LUX'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Lyon',
+        iataCode: 'LYS'
+      },
+      {
+        countryName: 'Portugal',
+        countryCode: 'PT',
+        iataName: 'Madeira',
+        iataCode: 'FNC'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Madrid',
+        iataCode: 'MAD'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Málaga',
+        iataCode: 'AGP'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Mallorca',
+        iataCode: 'PMI'
+      },
+      {
+        countryName: 'Malta',
+        countryCode: 'MT',
+        iataName: 'Malta',
+        iataCode: 'MLA'
+      },
+      {
+        countryName: 'Reino Unido',
+        countryCode: 'GB',
+        iataName: 'Manchester',
+        iataCode: 'MAN'
+      },
+      {
+        countryName: 'Marruecos',
+        countryCode: 'MA',
+        iataName: 'Marrakech',
+        iataCode: 'RAK'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Marsella',
+        iataCode: 'MRS'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Menorca',
+        iataCode: 'MAH'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Mikonos',
+        iataCode: 'JMK'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Milán',
+        iataCode: 'MXP'
+      },
+      {
+        countryName: 'Bielorrusia',
+        countryCode: 'BY',
+        iataName: 'Minsk',
+        iataCode: 'MSQ'
+      },
+      {
+        countryName: 'Rusia',
+        countryCode: 'RU',
+        iataName: 'Moscú(Domodedovo)',
+        iataCode: 'DME'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Múnich',
+        iataCode: 'MUC'
+      },
+      {
+        countryName: 'Marruecos',
+        countryCode: 'MA',
+        iataName: 'Nador',
+        iataCode: 'NDR'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Nantes',
+        iataCode: 'NTE'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Nápoles',
+        iataCode: 'NAP'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Niza',
+        iataCode: 'NCE'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Nuremberg',
+        iataCode: 'NUE'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Olbia',
+        iataCode: 'OLB'
+      },
+      {
+        countryName: 'Portugal',
+        countryCode: 'PT',
+        iataName: 'Oporto',
+        iataCode: 'OPO'
+      },
+      {
+        countryName: 'Argelia',
+        countryCode: 'DZ',
+        iataName: 'Orán',
+        iataCode: 'ORN'
+      },
+      {
+        countryName: 'Noruega',
+        countryCode: 'NO',
+        iataName: 'Oslo',
+        iataCode: 'OSL'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Palermo',
+        iataCode: 'PMO'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Pamplona',
+        iataCode: 'PNA'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Paris(Charles de Gaulle)',
+        iataCode: 'CDG'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'París(Orly)',
+        iataCode: 'ORY'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Pisa(Toscana)',
+        iataCode: 'PSA'
+      },
+      {
+        countryName: 'República Checa',
+        countryCode: 'CZ',
+        iataName: 'Praga',
+        iataCode: 'PRG'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Préveza',
+        iataCode: 'PVK'
+      },
+      {
+        countryName: 'Islandia',
+        countryCode: 'IS',
+        iataName: 'Reikiavik',
+        iataCode: 'KEF'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Rennes',
+        iataCode: 'RNS'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Rodas',
+        iataCode: 'RHO'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Roma(Fiumicino)',
+        iataCode: 'FCO'
+      },
+      {
+        countryName: 'Países Bajos',
+        countryCode: 'NL',
+        iataName: 'Róterdam',
+        iataCode: 'RTM'
+      },
+      {
+        countryName: 'Rusia',
+        countryCode: 'RU',
+        iataName: 'San Peters burgo',
+        iataCode: 'LED'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'San Sebastián',
+        iataCode: 'EAS'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Santander',
+        iataCode: 'SDR'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Santiago',
+        iataCode: 'SCQ'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Santorini',
+        iataCode: 'JTR'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Sevilla',
+        iataCode: 'SVQ'
+      },
+      {
+        countryName: 'Croacia',
+        countryCode: 'HR',
+        iataName: 'Split',
+        iataCode: 'SPU'
+      },
+      {
+        countryName: 'Alemania',
+        countryCode: 'DE',
+        iataName: 'Stuttgart',
+        iataCode: 'STR'
+      },
+      {
+        countryName: 'Estonia',
+        countryCode: 'EE',
+        iataName: 'Tallin',
+        iataCode: 'TLL'
+      },
+      {
+        countryName: 'Marruecos',
+        countryCode: 'MA',
+        iataName: 'Tánger',
+        iataCode: 'TNG'
+      },
+      {
+        countryName: 'Israel',
+        countryCode: 'IL',
+        iataName: 'TelAviv',
+        iataCode: 'TLV'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Tenerife Norte',
+        iataCode: 'TFN'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Tenerife Sur',
+        iataCode: 'TFS'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Tesalónica',
+        iataCode: 'SKG'
+      },
+      {
+        countryName: 'Francia',
+        countryCode: 'FR',
+        iataName: 'Toulouse',
+        iataCode: 'TLS'
+      },
+      {
+        countryName: 'Túnez',
+        countryCode: 'TN',
+        iataName: 'Túnez',
+        iataCode: 'TUN'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Turin',
+        iataCode: 'TRN'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Valencia',
+        iataCode: 'VLC'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Valladolid',
+        iataCode: 'VLL'
+      },
+      {
+        countryName: 'Polonia',
+        countryCode: 'PL',
+        iataName: 'Varsovia',
+        iataCode: 'WAW'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Venecia',
+        iataCode: 'VCE'
+      },
+      {
+        countryName: 'Italia',
+        countryCode: 'IT',
+        iataName: 'Verona',
+        iataCode: 'VRN'
+      },
+      {
+        countryName: 'Austria',
+        countryCode: 'AT',
+        iataName: 'Viena',
+        iataCode: 'VIE'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Vigo',
+        iataCode: 'VGO'
+      },
+      {
+        countryName: 'Croacia',
+        countryCode: 'HR',
+        iataName: 'Zadar',
+        iataCode: 'ZAD'
+      },
+      {
+        countryName: 'Croacia',
+        countryCode: 'HR',
+        iataName: 'Zagreb',
+        iataCode: 'ZAG'
+      },
+      {
+        countryName: 'Grecia',
+        countryCode: 'GR',
+        iataName: 'Zante',
+        iataCode: 'ZTH'
+      },
+      {
+        countryName: 'España',
+        countryCode: 'ES',
+        iataName: 'Zaragoza',
+        iataCode: 'ZAZ'
+      },
+      {
+        countryName: 'Suiza',
+        countryCode: 'CH',
+        iataName: 'Zúrich',
+        iataCode: 'ZRH'
+      },
+      {
+        countryName: 'Argentina',
+        countryCode: 'AR',
+        iataName: 'BuenosAires',
+        iataCode: 'EZE'
+      },
+      {
+        countryName: 'Estados Unidos(EE.UU)',
+        countryCode: 'US',
+        iataName: 'LosÁngeles',
+        iataCode: 'LAX'
+      },
+      {
+        countryName: 'Estados Unidos(EE.UU)',
+        countryCode: 'US',
+        iataName: 'Oakland San Francisco Bay',
+        iataCode: 'OAK'
+      },
+      {
+        countryName: 'República Dominicana',
+        countryCode: 'DO',
+        iataName: 'PuntaCana',
+        iataCode: 'PUJ'
+      }
+    ]
+  }
+}
