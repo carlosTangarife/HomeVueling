df5b71803b1f8c1a1ebe95f86f983bffc6399f73
andresfm87@gmail.com <andresfm87@gmail.com>
2017-09-07 13:00:50 -0500

    PRJ-5138: functionality pipe was described

--- src/app/search/components/passenger/discount-passenger/discount-passenger.pipe.ts
+++ src/app/search/components/passenger/discount-passenger/discount-passenger.pipe.ts
@@ -4,6 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'discountPassenger'
 })
 
+/*was created the pipe, this gives a special formatting to the discount and returns it in this format (65%) */
 export class DiscountPassengerPipe implements PipeTransform {
   transform(value: any): string {
     if (value) {
