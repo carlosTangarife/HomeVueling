8fdc6ed33ebd3f76cc367e5d9184277b2c90d8b4
andresfm87@gmail.com <andresfm87@gmail.com>
2017-09-07 12:58:22 -0500

    PRJ-2138: unnecessary css classes were removed

--- src/app/search/components/calendar/calendar.component.ts
+++ src/app/search/components/calendar/calendar.component.ts
@@ -7,16 +7,16 @@ import { Component, OnInit } from '@angular/core';
     <div class="col col-md-6">
       <label>Ida</label>
       <div class="input-group">
-        <input type="text" id="datepicker" class="form-control js-searchbar-input js-searchbar-date-input" placeholder="DD/MM/YY" readonly="readonly">
+        <input type="text" id="datepicker" class="form-control" placeholder="DD/MM/YY" readonly="readonly">
         <span class="icon icon-calendar"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
       </div>
     </div>
     <div class="col col-md-6">
-      <a class="activate js-add-date-range-to">Añadir vuelta</a>
-      <div class="date-range-to js-date-range-to">
+      <a class="activate ">Añadir vuelta</a>
+      <div class="date-range-to ">
         <label>Vuelta</label>
         <div class="input-group">
-          <input type="text" id="datepicker" class="form-control js-searchbar-input js-searchbar-date-input" placeholder="DD/MM/YY" readonly="readonly">
+          <input type="text" id="datepicker" class="form-control" placeholder="DD/MM/YY" readonly="readonly">
           <span class="icon icon-calendar"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>
         </div>
       </div>
