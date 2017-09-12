f043e0d7a9047320cca589021915164aa3860473
blacksagit <andresfm87@gmail.com>
2017-09-12 09:50:45 -0500

    PRJ-5138: fix pax rules when adults is zero

--- src/app/search/consts/passenger.ts
+++ src/app/search/consts/passenger.ts
@@ -1,8 +1,15 @@
 import { ITypePassenger } from '../models/passenger.model';
 
+export const enum TYPE_PASSENGER {
+    adults,
+    children,
+    infants,
+    extraSeat
+};
+
 export const TYPE_PAX_LIST: Array<ITypePassenger> = [
-    {label: 'adults', rulAge: 'adultCaption', type: 'adults'},
-    {label: 'children', rulAge: 'childrenCaption', type: 'children'},
-    {label: 'infants', rulAge: 'infantCaption', type: 'infants'},
-    {label: 'extraseat', rulAge: 'plusMoreInfo', type: 'extraSeat'}
+    {label: 'adults', rulAge: 'adultCaption', type: TYPE_PASSENGER.adults.toString()},
+    {label: 'children', rulAge: 'childrenCaption', type: TYPE_PASSENGER.children.toString()},
+    {label: 'infants', rulAge: 'infantCaption', type: TYPE_PASSENGER.infants.toString()},
+    {label: 'extraseat', rulAge: 'plusMoreInfo', type: TYPE_PASSENGER.extraSeat.toString()}
 ];
--- src/app/search/services/passenger.service.ts
+++ src/app/search/services/passenger.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { ConfigService } from '../../shared/services/config.service';
 import { IRulesPassenger, IMaxAndMinRule } from '../models/rules-passenger.model';
 import { IPassengers, ITypePassenger } from '../models/passenger.model';
-import { TYPE_PAX_LIST } from '../consts/passenger';
+import { TYPE_PAX_LIST, TYPE_PASSENGER } from '../consts/passenger';
 
 @Injectable()
 export class PassengerService {
@@ -63,39 +63,26 @@ export class PassengerService {
   }
 
   private ruleAdults(passenger: IPassengers, rule: IMaxAndMinRule) {
-    this.ruleValidate(passenger.adults, rule.min, rule.max, 'adults');
+    this.ruleValidate(passenger.adults, rule.min, rule.max, TYPE_PASSENGER.adults.toString());
   }
 
   private ruleChildren(passenger: IPassengers, rule: IMaxAndMinRule) {
     let max = (rule.maxWhenAdults && passenger.adults > 0) ? rule.maxWhenAdults : rule.max;
-    this.ruleValidate(passenger.children, rule.min, max, 'children');
+    this.ruleValidate(passenger.children, rule.min, max, TYPE_PASSENGER.children.toString());
   }
 
   private ruleInfants(passenger: IPassengers, rule: IMaxAndMinRule) {
-    let pax = 0;
-    if (rule.dependent) {
-      rule.dependent.forEach(type => {
-        pax += passenger[type];
-      });
-    }
-    let max = (pax === 0 || pax > rule.max) ? rule.max : pax;
-    this.ruleValidate(passenger.infants, rule.min, max, 'infants');
+    let max = this.getRuleMax(passenger, rule);
+    this.ruleValidate(passenger.infants, rule.min, max, TYPE_PASSENGER.infants.toString());
   }
 
   private ruleExtraSeat(passenger: IPassengers, rule: IMaxAndMinRule) {
-    let pax = 0;
-    if (rule.dependent) {
-      rule.dependent.forEach(type => {
-        pax += passenger[type];
-      });
-    }
-    let max = (pax === 0 || pax > rule.max) ? rule.max : pax;
-    this.ruleValidate(passenger.extraSeat, rule.min, max, 'extraSeat');
+    let max = this.getRuleMax(passenger, rule);
+    this.ruleValidate(passenger.extraSeat, rule.min, max, TYPE_PASSENGER.extraSeat.toString());
   }
 
   private rulePassenger(passenger) {
     passenger.totalPassengers = passenger.adults + passenger.children;
-
     if (passenger.totalPassengers > this.configPassenger.max) {
       window.location.href = this.configPassenger.urlmax;
     }
@@ -111,5 +98,16 @@ export class PassengerService {
       value: this.values[key].observer
     };
     this.typePassengerListSubject.next(this.typePassengerList);
+  }
+
+  private getRuleMax(passenger: IPassengers, rule: IMaxAndMinRule): number {
+    let pax = rule.max + 1;
+    if (rule.dependent) {
+      pax = 0;
+      rule.dependent.forEach(type => {
+        pax += passenger[type];
+      });
+    }
+    return (pax > rule.max) ? rule.max : pax;
   }
 }
