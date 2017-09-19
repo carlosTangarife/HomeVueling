a35fb5ee33ebdd801402ea42860574eb0a2213ac
blacksagit <andresfm87@gmail.com>
2017-09-19 11:14:06 -0500

    PRJ-5138: fix error consoles and ts lint

--- src/app/search/errors/check-in-error/check-in-error.component.ts
+++ src/app/search/errors/check-in-error/check-in-error.component.ts
@@ -1,35 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { IContactPhones } from "../../models/contact-phones.model";
-import { ConfigService } from "../../../shared/services/config.service";
-import { SelectorService } from "../../../shared/services/selector.service";
+import { IContactPhones } from '../../models/contact-phones.model';
+import { ConfigService } from '../../../shared/services/config.service';
+import { SelectorService } from '../../../shared/services/selector.service';
 
 @Component({
   selector: '[app-check-in-error]',
-  templateUrl: './check-in-error.component.html'  
+  templateUrl: './check-in-error.component.html'
 })
 export class CheckInErrorComponent implements OnInit {
   public submit: boolean;
-  public countryInfo: Array<Object>;   
+  public countryInfo: Array<Object>;
   public selectDevice: Object;
-  
+
   constructor(public selectorService: SelectorService) {
-    this.submit = false;    
-    this.countryInfo= [
-      {countryCode: "DE", name: "Alemania"},
-      {countryCode: "AU", name: "Austria"},
-      {countryCode: "BE", name: "Bélgica"},
-      {countryCode: "ES", name: "España"},
-      {countryCode: "FR", name: "Francia"},
-      {countryCode: "NL", name: "Holanda"},
-      {countryCode: "IT", name: "Italia"},
-      {countryCode: "PT", name: "Portugal"},
-      {countryCode: "GB", name: "Reino Unido"},
-      {countryCode: "RU", name: "Rusia"},
-      {countryCode: "SW", name: "Suiza"},
-      {countryCode: "WW", name: "Resto del mundo"}
+    this.submit = false;
+    this.countryInfo = [
+      {countryCode: 'DE', name: 'Alemania'},
+      {countryCode: 'AU', name: 'Austria'},
+      {countryCode: 'BE', name: 'Bélgica'},
+      {countryCode: 'ES', name: 'España'},
+      {countryCode: 'FR', name: 'Francia'},
+      {countryCode: 'NL', name: 'Holanda'},
+      {countryCode: 'IT', name: 'Italia'},
+      {countryCode: 'PT', name: 'Portugal'},
+      {countryCode: 'GB', name: 'Reino Unido'},
+      {countryCode: 'RU', name: 'Rusia'},
+      {countryCode: 'SW', name: 'Suiza'},
+      {countryCode: 'WW', name: 'Resto del mundo'}
     ];
     this.selectDevice = this.countryInfo[3];
-        
    }
 
   ngOnInit() {
@@ -37,18 +36,16 @@ export class CheckInErrorComponent implements OnInit {
     this.selectorService.loadContactPhones('BE');
   }
 
-  onSubmit(){    
+  onSubmit() {
     this.submit = !this.submit;
   }
 
-  onChange(newValue){
+  onChange(newValue) {
     console.log(newValue);
-    this.selectDevice = newValue; 
+    this.selectDevice = newValue;
   }
 
-  getContactPhones(code: string){          
-      
-    }  
+  getContactPhones(code: string) {
+
   }
-
-
+}
