bde035976b3551e64f0eb547d86eea99a039e1be
andresfm87@gmail.com <andresfm87@gmail.com>
2017-09-26 14:22:43 -0500

    PRJ-5138: component checkin was copy and pasted in reservation component

--- src/app/search/components/reservation/reservation.component.ts
+++ src/app/search/components/reservation/reservation.component.ts
@@ -1,14 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
+import { environment } from '../../../../environments/environment';
+
+/*Local Services */
+import { CalendarService } from '../../../shared/services/calendar.service';
+import { SelectorService } from '../../../shared/services/selector.service';
+import { CheckInService } from '../check-in/check-in.service';
+
+/*Models using interface */
+import { IStation } from '../../../shared/models/station.model';
+import { ICheckIn } from '../../models/check-in.model';
 
 @Component({
   selector: '[app-reservation]',
-  templateUrl: './reservation.component.html'
+  templateUrl: './reservation.component.html',
+  providers: [CheckInService, SelectorService]
 })
 export class ReservationComponent implements OnInit {
+  public codeBooking: string;
+  public email: string;
+  public keyCookie: string;
+  public isEmail: boolean;
+  public isOriginDestination: boolean;
+  public isShowStation: boolean;
+  public station: IStation;
+  public validation: boolean;
+  public flightTomorrow: Date;
 
-  constructor() { }
+  @Output() selectedOrigin: EventEmitter<string> = new EventEmitter();
+
+  constructor(public checkInService: CheckInService, public selectorService: SelectorService, public calendarService: CalendarService) {
+    this.isEmail = true;
+    this.isOriginDestination = false;
+    this.isShowStation = false;
+    this.keyCookie = environment.keyCheckInCookie;
+    this.validation = false;
+    this.flightTomorrow = null;
+   }
 
   ngOnInit() {
+    this.selectorService.loadStations();
+    this.codeBooking = this.checkInService.getCodeBooking(this.keyCookie);
   }
 
+  showEmail() {
+    this.isEmail = true;
+    this.isOriginDestination = false;
+  }
+
+  showOriginDestination() {
+    this.isOriginDestination = true;
+    this.isEmail = false;
+  }
+
+  showStation() {
+    this.isShowStation = !this.isShowStation;
+  }
+
+  stationSelected(station: any) {
+    this.station = station.name;
+  }
+
+  showCalendar() {
+    this.calendarService.toggleShowDatePicker();
+  }
+
+  selectedDate(date: Date) {
+    this.flightTomorrow = date;
+    this.calendarService.toggleShowDatePicker();
+  }
+
+  myFlightTomorrow() {
+    this.flightTomorrow = new Date();
+    this.flightTomorrow.setDate(this.flightTomorrow.getDate() + 1);
+  }
+
+  onSubmit(forma: NgForm) {
+    this.validation = true;
+  }
 }
