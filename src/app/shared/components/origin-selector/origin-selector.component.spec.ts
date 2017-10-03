e4c345114ea8915869124e97798bd582ec9eb7f8
blacksagit <andresfm87@gmail.com>
2017-10-03 06:45:38 -0500

    PRJ-5138: origin-selector and places component testing was initialized

--- src/app/shared/components/origin-selector/origin-selector.component.spec.ts
+++ src/app/shared/components/origin-selector/origin-selector.component.spec.ts
@@ -1,25 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OriginSelectorComponent } from './origin-selector.component';
+import { SelectorService } from '../../services/selector.service';
+import { ConfigService } from '../../services/config.service';
+import { StationService } from '../../services/station.service';
+import { DictionaryPipe } from '../../pipes/dictionary.pipe';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('OriginSelectorComponent', () => {
+fdescribe('OriginSelectorComponent', () => {
+  debugger;
   let component: OriginSelectorComponent;
-  let fixture: ComponentFixture<OriginSelectorComponent>;
+  let fixture: ComponentFixture<OriginSelectorComponent>
+
+  let configService = {
+    getDictionary(key: string) {
+      return '';
+    }
+  }
+
+  let selectorService = {
+    loadListStations(isOrigin: boolean) {
+      return true;
+    },
+    getStations(isOrigin: boolean) {
+      return {code: 'LCG', countryCode: 'ES', countryName: 'España', isRecent: false, macCode: '',
+      name: 'A Coruña', order: -1 };
+    }
+  }
+
+  let stationService = {
+    getCookieStations(key: string) {
+      return {iataCode: 'LCG', Date: new Date()}
+    }
+  }
 
   beforeEach(async(() => {
+    debugger;
     TestBed.configureTestingModule({
-      declarations: [ OriginSelectorComponent ]
+      declarations: [ OriginSelectorComponent, DictionaryPipe ],
+      providers: [
+                  {provide: SelectorService, useValue: selectorService},
+                  {provide: ConfigService, useValue: configService},
+                  {provide: StationService, useValue: stationService}
+                    ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
-    .compileComponents();
+    // .compileComponents();
+    fixture = TestBed.createComponent(OriginSelectorComponent);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OriginSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(OriginSelectorComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  fit('should be created', () => {
+    debugger;
+    expect(fixture).toBeTruthy();
   });
 });
--- src/app/shared/components/places/places.component.spec.ts
+++ src/app/shared/components/places/places.component.spec.ts
@@ -3,28 +3,30 @@ import { PlacesComponent } from './places.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { DictionaryPipe } from '../../pipes/dictionary.pipe';
 import { ConfigService } from '../../services/config.service';
-// import { ResourcesService } from '../../services/resources.service';
-// import { LoggerService } from '../../services/logger.service';
-// import { StorageService } from '../../services/storage.service';
-// import { PersistenceService } from 'angular-persistence';
-// import { Http } from '@angular/http';
+import { ResourcesService } from '../../services/resources.service';
+import { LoggerService } from '../../services/logger.service';
+import { StorageService } from '../../services/storage.service';
+import { PersistenceService } from 'angular-persistence';
+import { Http } from '@angular/http';
 
 fdescribe('PlacesComponent', () => {
   let component: PlacesComponent;
   let fixture: ComponentFixture<PlacesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     debugger;
     TestBed.configureTestingModule({
+      imports:  [ Http ],
       declarations: [ PlacesComponent, DictionaryPipe ],
-      providers: [ ConfigService ],
+      providers: [ ConfigService, ResourcesService, LoggerService, StorageService, PersistenceService ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
-
-  }));
+    fixture = TestBed.createComponent(PlacesComponent)
+  });
 
   beforeEach(() => {
+    debugger;
     fixture = TestBed.createComponent(PlacesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
