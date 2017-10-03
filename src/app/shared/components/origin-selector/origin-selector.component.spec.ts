a511457c174260e257c8883578f1200e8e7bb801
blacksagit <andresfm87@gmail.com>
2017-10-03 11:57:39 -0500

    PRJ-5138: The test of the components origin-selector and places was finalized (should be created). Was created testing folder with the mocks of services

--- src/app/shared/components/origin-selector/origin-selector.component.spec.ts
+++ src/app/shared/components/origin-selector/origin-selector.component.spec.ts
@@ -1,61 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OriginSelectorComponent } from './origin-selector.component';
-import { SelectorService } from '../../services/selector.service';
+import { DictionaryPipe } from '../../pipes/dictionary.pipe';
 import { ConfigService } from '../../services/config.service';
 import { StationService } from '../../services/station.service';
-import { DictionaryPipe } from '../../pipes/dictionary.pipe';
+import { SelectorService } from '../../services/selector.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockDictionary, MockStationService, MockSelectorService } from '../testing/mocks';
+
 
 fdescribe('OriginSelectorComponent', () => {
   debugger;
   let component: OriginSelectorComponent;
   let fixture: ComponentFixture<OriginSelectorComponent>
 
-  let configService = {
-    getDictionary(key: string) {
-      return '';
-    }
-  }
+  let configService: ConfigService;
+  let stationService: StationService;
+  let selectorService: SelectorService;
 
-  let selectorService = {
-    loadListStations(isOrigin: boolean) {
-      return true;
-    },
-    getStations(isOrigin: boolean) {
-      return {code: 'LCG', countryCode: 'ES', countryName: 'España', isRecent: false, macCode: '',
-      name: 'A Coruña', order: -1 };
-    }
-  }
-
-  let stationService = {
-    getCookieStations(key: string) {
-      return {iataCode: 'LCG', Date: new Date()}
-    }
-  }
-
-  beforeEach(async(() => {
-    debugger;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ OriginSelectorComponent, DictionaryPipe ],
       providers: [
-                  {provide: SelectorService, useValue: selectorService},
-                  {provide: ConfigService, useValue: configService},
-                  {provide: StationService, useValue: stationService}
-                    ],
+        {provide: ConfigService, useClass: MockDictionary },
+        {provide: StationService, useClass: MockStationService},
+        {provide: SelectorService, useClass: MockSelectorService}
+      ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
-    // .compileComponents();
-    fixture = TestBed.createComponent(OriginSelectorComponent);
-  }));
+    .overrideComponent(OriginSelectorComponent, {
+      set: {
+        providers: [
+          {provide: SelectorService, useClass: MockSelectorService}
+            ]
+      }
+    });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(OriginSelectorComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+    component = TestBed.createComponent(OriginSelectorComponent).componentInstance;
+    configService = TestBed.get(ConfigService);
+    stationService = TestBed.get(StationService);
+    selectorService = TestBed.get(SelectorService);
+  });
 
   fit('should be created', () => {
-    debugger;
-    expect(fixture).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
 });
--- src/app/shared/components/places/places.component.spec.ts
+++ src/app/shared/components/places/places.component.spec.ts
@@ -3,41 +3,24 @@ import { PlacesComponent } from './places.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { DictionaryPipe } from '../../pipes/dictionary.pipe';
 import { ConfigService } from '../../services/config.service';
-import { ResourcesService } from '../../services/resources.service';
-import { LoggerService } from '../../services/logger.service';
-import { StorageService } from '../../services/storage.service';
-import { PersistenceService } from 'angular-persistence';
-import { Http } from '@angular/http';
+import { MockDictionary } from '../testing/mocks';
 
 fdescribe('PlacesComponent', () => {
   let component: PlacesComponent;
   let fixture: ComponentFixture<PlacesComponent>;
+  let configService: ConfigService;
 
-<<<<<<< HEAD
   beforeEach(() => {
-    debugger;
-=======
-  beforeEach(async(() => {
->>>>>>> ee925976f3be319c02087b5f1f6e85ae39fb5658
     TestBed.configureTestingModule({
-      imports:  [ Http ],
       declarations: [ PlacesComponent, DictionaryPipe ],
-      providers: [ ConfigService, ResourcesService, LoggerService, StorageService, PersistenceService ],
+      providers: [ {provide: ConfigService, useClass: MockDictionary} ],
       schemas: [ NO_ERRORS_SCHEMA ]
-    })
-    .compileComponents();
-    fixture = TestBed.createComponent(PlacesComponent)
-  });
-
-  beforeEach(() => {
-    debugger;
-    fixture = TestBed.createComponent(PlacesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
     });
+    component = TestBed.createComponent(PlacesComponent).componentInstance;
+    configService = TestBed.get(ConfigService);
+  });
 
   fit ('should be created', () => {
-    expect(fixture).toBeTruthy();
+    expect(component).toBeTruthy();
   });
-
 });
